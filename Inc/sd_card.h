73f20f106b00715f2a185299a816ee2cba5aa7ad
raymonddong <raymonddonghome@gmail.com>
2019-02-20 23:44:45 -0500

    init sd_card

---
+++ Inc/sd_card.h
@@ -0,0 +1,15 @@
+/*
+ * sd_card.h
+ *
+ *  Created on: Feb 20, 2019
+ *      Author: raymo
+ */
+
+#ifndef SD_CARD_H_
+#define SD_CARD_H_
+
+#include "bms.h"
+
+#define SD_CARD_RATE       500 / portTICK_RATE_MS // @matt
+
+#endif /* SD_CARD_H_ */
---
+++ Src/sd_card.c
@@ -0,0 +1,20 @@
+/*
+ * sd_card.c
+ *
+ *  Created on: Feb 20, 2019
+ *      Author: raymo
+ */
+#include "sd_card.h"
+
+void task_sd_card() {
+  init_sd_card();
+  TickType_t time_init = 0;
+  while (1) {
+    time_init = xTaskGetTickCount();
+    vTaskDelayUntil(&time_init, SD_CARD_RATE);	// @matt what is this supposed to be?
+  }
+}
+
+void init_sd_card(){
+
+}
