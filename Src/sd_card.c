5e1620fba88578fc7e49640fdff17e695fa43593
raymonddong <raymonddonghome@gmail.com>
2019-02-20 23:58:44 -0500

    more init stuff yo

--- Src/sd_card.c
+++ Src/sd_card.c
@@ -16,5 +16,25 @@ void task_sd_card() {
 }
 
 void init_sd_card(){
+	FATFS SDFatFs;  /* File system object for SD disk logical drive */
+	FIL MyFile;     /* File object */
+	char SDPath[4]; /* SD disk logical drive path */
+	uint8_t workBuffer[_MAX_SS];
 
+	if(FATFS_LinkDriver(&SD_Driver, SDPath) == 0){
+	    if(f_mount(&SDFatFs, (TCHAR const*)SDPath, 0) != FR_OK)
+	    {
+	    	;// error handling
+	    }
+	    else{
+	    	/*##-4- Create and Open a new text file object with write access #####*/
+			if(f_open(&MyFile, "STM32.TXT", FA_CREATE_ALWAYS | FA_WRITE) != FR_OK)
+			{
+				;// error handling
+			}
+	    }
+	}
+	else{
+		;// not linked
+	}
 }
