dbf9ddcf8828a293b37fc1e1265b972f6afa71af
raymonddong <raymonddonghome@gmail.com>
2019-02-24 17:22:44 -0500

    stash

--- Src/sd_card.c
+++ Src/sd_card.c
@@ -8,11 +8,36 @@
 
 void task_sd_card() {
   init_sd_card();
+  /*Code not needed for demo
   TickType_t time_init = 0;
   while (1) {
     time_init = xTaskGetTickCount();
+
     vTaskDelayUntil(&time_init, SD_CARD_RATE);	// @matt what is this supposed to be?
   }
+  */
+  FRESULT res;                                          /* FatFs function common result code */
+  uint32_t byteswritten, bytesread;                     /* File write/read counts */
+  uint8_t wtext[] = "This is STM32 working with FatFs"; /* File write buffer */
+  uint8_t rtext[100];                                   /* File read buffer */
+  FIL MyFile;     /* File object */
+  if(f_open(&MyFile, "STM32.TXT", FA_CREATE_ALWAYS | FA_WRITE) != FR_OK)
+  {
+	return;/* 'STM32.TXT' file Open for write Error */
+  }
+  else
+  {
+	/*##-5- Write data to the text file ################################*/
+	res = f_write(&MyFile, wtext, sizeof(wtext), (void *)&byteswritten);
+	if((byteswritten == 0) || (res != FR_OK))
+	{
+	  return;/* 'STM32.TXT' file Write or EOF Error */
+	}
+	else
+	{
+	  /*##-6- Close the open text file #################################*/
+	  f_close(&MyFile);
+	}
 }
 
 void init_sd_card(){
@@ -27,14 +52,15 @@ void init_sd_card(){
 	    	;// error handling
 	    }
 	    else{
-	    	/*##-4- Create and Open a new text file object with write access #####*/
-			if(f_open(&MyFile, "STM32.TXT", FA_CREATE_ALWAYS | FA_WRITE) != FR_OK)
-			{
-				;// error handling
-			}
+	    	if (f_mkfs((TCHAR const*)SDPath, FM_ANY, 0, workBuffer, sizeof(workBuffer)) != FR_OK){
+	    		;// error handling
+	    	}
+	    	else{
+	    		return;
+	    	}
 	    }
 	}
 	else{
-		;// not linked
+		;// error handling
 	}
 }
