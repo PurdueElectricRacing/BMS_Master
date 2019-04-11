5a68125f8b733d251a048667b2de387dc17844a6
raymonddong <raymonddonghome@gmail.com>
2019-04-11 15:33:21 -0400

    deleting comments and refactoring

--- Src/sd_card.c
+++ Src/sd_card.c
@@ -7,13 +7,9 @@
 #include "sd_card.h"
 
 void task_sd_card() {
-
-	uint8_t text0[] = "\n0x000, 0x0000000000000000";
-
 	//uint8_t workBuffer[_MAX_SS];
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
 	HAL_Delay(100);
-	//uint8_t workBuffer[_MAX_SS];
 	if (f_mount(&SDFatFS, (TCHAR const*) SDPath, 0) == FR_OK) {
 		//if (f_mkfs((TCHAR const*) SDPath, FM_ANY, 0, workBuffer, sizeof(workBuffer))
 		//		== FR_OK) {
@@ -28,30 +24,11 @@ void task_sd_card() {
 				task_sd_card_process();
 			}
 		}
-		/*
-		 if (f_open(&SDFileData, "DATA.TXT", FA_OPEN_APPEND | FA_WRITE) == FR_OK) {
-		 f_lseek(&SDFileData, f_size(&SDFileData));
-		 if (f_write(&SDFileData, text0, sizeof(text0), (void *) &wbytes) == FR_OK) {
-		 f_lseek(&SDFileData, f_size(&SDFileData));
-		 if (f_write(&SDFileData, text1, sizeof(text1), (void *) &wbytes) == FR_OK) {
-		 //HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
-		 f_close(&SDFileData);
-		 }
-		 }
-		 if (f_open(&SDFileSetting, "SETTING.TXT", FA_CREATE_ALWAYS | FA_WRITE) == FR_OK) {
-		 if (f_write(&SDFileSetting, sett0, sizeof(sett0), (void *) &wbytes) == FR_OK) {
-		 //HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
-		 f_close(&SDFileSetting);
-		 }
-		 }
-		 //f_unlink("STM32.TXT");// delete
-		 }
-		 */
 		//}
 	}
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
-	HAL_Delay(100);
+	HAL_Delay(500);
 	FATFS_UnLinkDriver(SDPath);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
@@ -64,7 +41,6 @@ void task_sd_card_process() {
 	char setting[] = "00, 0x0001\n01, 0x0000\n02, 0x0000\n03, 0x0000\n"
 			"04, 0x00000000\n05, 0x00000000\n06, 0x0000\n07, 0x0000\n"
 			"08, 0x0000\n09, 0x0000\n10, 0x0000\n16, 0x0000";
-
 	sdcard_t msg;
 	strncpy(msg.id, "000", 3);
 	strncpy(msg.data, "0000000000000000", 16);
@@ -73,22 +49,6 @@ void task_sd_card_process() {
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
 	int a = 0;
 	int b = 0;
-	/*
-	 for (int i = 0; i < 12; i++){
-	 task_sd_card_get_setting(setting, i);
-	 }
-	 f_write(&SDFileSetting, setting, sizeof(setting), (void *) &wbytes);
-	 */
-	/*
-	 sprintf(t0, "\n0x%s", "000");
-	 sprintf(t1, ", 0x%s", "0123456701234567");
-	 strcpy(t0, strcat(t0, t1));
-	 f_lseek(&SDFileData, f_size(&SDFileData));
-	 f_write(&SDFileData, t0, sizeof(t0), (void *) &wbytes);
-	 f_close(&SDFileData);
-	 f_close(&SDFileSetting);
-	 return;
-	 */
 	while (1) {
 		time_init = xTaskGetTickCount();
 		if (xQueuePeek(bms.q_sd_card, &msg, TIMEOUT) == pdTRUE) {
@@ -106,10 +66,10 @@ void task_sd_card_process() {
 				sprintf(t0, "\n0x%s", "000");
 				sprintf(t1, ", 0x%s", "0123456701234567");
 				strcpy(t0, strcat(t0, t1));
-				if (b < 3){
-				f_lseek(&SDFileData, f_size(&SDFileData));
-				f_write(&SDFileData, t0, sizeof(t0), (void *) &wbytes);
-				b++;
+				if (b < 3) {
+					f_lseek(&SDFileData, f_size(&SDFileData));
+					f_write(&SDFileData, t0, sizeof(t0), (void *) &wbytes);
+					b++;
 				}
 				break;
 			default:
@@ -117,22 +77,22 @@ void task_sd_card_process() {
 			}
 		}
 
-		 if (b == 3) {
-		 msg.file = 0;
-		 xQueueSendToBack(bms.q_sd_card, &msg, TIMEOUT);
-		 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
-		 b = 0;
-		 }
-		 if (a == 3) {
-		 strncpy(msg.id, "001", 3);
-		 strncpy(msg.data, "0123456700001111", 16);
-		 msg.file = 1;
-		 xQueueSendToBack(bms.q_sd_card, &msg, TIMEOUT);
-		 a = 0;
-		 } else {
-		 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
-		 a++;
-		 }
+		if (b == 3) {
+			msg.file = 0;
+			xQueueSendToBack(bms.q_sd_card, &msg, TIMEOUT);
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+			b = 0;
+		}
+		if (a == 3) {
+			strncpy(msg.id, "001", 3);
+			strncpy(msg.data, "0123456700001111", 16);
+			msg.file = 1;
+			xQueueSendToBack(bms.q_sd_card, &msg, TIMEOUT);
+			a = 0;
+		} else {
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
+			a++;
+		}
 
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
 		vTaskDelayUntil(&time_init, 1000);
